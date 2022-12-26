039f8d1f4fde41dbdc80cb5374c7d09635c18929
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-12-26 19:48:42 +0530

    modified dashboard styles

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -13,7 +13,6 @@ const DashBoard = () => {
       className={`flex md:flex-row flex-col ${styles.paddingY}`}
     >
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
       <div
         className={`bg-primary flex flex-row ${styles.paddingX} ${styles.flexCenter}`}
