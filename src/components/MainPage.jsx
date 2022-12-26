b6d726dc0ce0f6d178a841884731f0a8eeda9fb5
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-12-26 08:18:19 +0530

    made responsive navbar

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import React from "react";
 import { navLinks } from "../constants";
+import close from "../assets/close.svg";
+import menu from "../assets/menu.svg";
 
 const MainPage = () => {
   const [active, setActive] = useState("Home");
@@ -26,6 +28,33 @@ const MainPage = () => {
           </li>
         ))}
       </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar text-white`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,14 +1,18 @@
 export const navLinks = [
   {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "my appointments",
+    title: "My Appointments",
+  },
+  {
     id: "team",
     title: "Team",
   },
   {
     id: "source code",
     title: "Source Code",
-  },
-  {
-    id: "my appointments",
-    title: "My Appointments",
   },
 ];
