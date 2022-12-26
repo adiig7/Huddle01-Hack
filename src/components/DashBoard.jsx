fa3a212494e3b0402f44d35d47d823e41ef373a2
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-12-26 19:43:43 +0530

    modified card

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -15,8 +15,26 @@ const DashBoard = () => {
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
       <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
+      <div
+        className={`bg-primary flex flex-row ${styles.paddingX} ${styles.flexCenter}`}
+      >
+        <div className={`flex flex-row gap-2.5 ${styles.boxWidth}`}>
+          <DoctorCard
+            image={people01}
+            name="Herman Jensen"
+            category="Therapist"
+            price="$200"
+            desc="Hey there! I am your therapist Hermn Jensen.We all need a little discipline. Exercise is my discipline"
+            rate={star}
+          />
+          <DoctorCard
+            image={people01}
+            name="Herman Jensen"
+            category="Therapist"
+            price="$200"
+            desc="Hey there! I am your therapist Hermn Jensen.We all need a little discipline. Exercise is my discipline"
+            rate={star}
+          />
           <DoctorCard
             image={people01}
             name="Herman Jensen"
