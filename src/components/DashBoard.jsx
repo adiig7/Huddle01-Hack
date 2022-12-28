4790b6e4af5b4a37294ac88b77115a667d115881
Aditya Gupta <adityagupta765469@gmail.com>
2022-12-28 22:28:20 +0530

    add redirect on login

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -55,7 +55,7 @@ const DashBoard = () => {
 
   useEffect(() => {
     getAllDocsData();
-  })
+  }, [])
 
   return (
     <section
--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -6,7 +6,6 @@ const DoctorCard = (props) => {
       <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
         <div className="flex flex-row">
           <img
-            onClick={getAllDocsData}
             src={props.image}
             className="w-[48px] h-[48px] rounded-full"
           ></img>
