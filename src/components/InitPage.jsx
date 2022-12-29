b961be4c7bad39ae7992539ccb46ec5695c1bc08
Aditya Gupta <adityagupta765469@gmail.com>
2022-12-29 17:41:03 +0530

    add logo on auth page

--- src/components/InitPage.jsx
+++ src/components/InitPage.jsx
@@ -2,6 +2,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React, { useEffect, useState } from "react";
 import styles from "../style";
 import ABI from "./../utils/abi"
+import logo from "../assets/logo.svg";
 import { useSigner, useContract, useProvider, useAccount } from "wagmi";
 import { useNavigate } from 'react-router-dom';
 
@@ -56,9 +57,8 @@ const InitPage = () => {
         <div className={`${styles.boxWidth}`}>
           <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
           <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-          <h1 className="text-white h-[150px] nav-heading text-7xl text-center mt-6 text-gradient font-bold">
-            User Registration
-          </h1>
+
+          <img src={logo} className="w-[150px] h-[150px] m-auto mt-4"></img>
 
           <div className="w-[100%] flex flex-col items-center justify-center m-auto mt-20">
             <ConnectButton />
