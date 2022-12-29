4f1250eded1a040517d14f824c9959352981cfef
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-12-29 14:55:39 +0530

    added team members

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import style from "../constants/team.css";
+import dhrumi_shah from "../assets/dhrumi_shah.jpg";
+import aditya_gupta from "../assets/aditya_gupta.jpg";
+import twitter from "../assets/twitter.png";
+import github from "../assets/github.svg";
 
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <div className="bg-primary w-full h-screen">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <div className="absolute z-[0] w-[40%] h-[45%] top-0 pink__gradient" />
+          <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+          <h1 className="text-white  nav-heading text-7xl text-center mt-6 text-gradient font-bold">
+            Team Members
+          </h1>
+          <div className="flex flex-row">
+            <div className="founders">
+              <div className="card">
+                <h2 className="text-gradient text-3xl font-bold mb-2">
+                  Dhrumi Shah
+                </h2>
 
-export default Team
+                <img
+                  src={dhrumi_shah}
+                  alt="Dhrumi Shah"
+                  className="w-[250px] h-[250px]"
+                />
+                <p className="text-white mt-4 mb-4 text-2xl">
+                  <b>Frontend Developer</b>
+                </p>
+
+                <div className="flex flex-row m-auto justify-center align-center">
+                  <a
+                    href="https://twitter.com/deetwts"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={twitter} alt="Twitter Logo" />
+                  </a>
+                  <a
+                    href="https://github.com/dhrumishah"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={github} alt="Github Logo" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="founders">
+              <div className="card">
+                <h2 className="text-gradient text-3xl font-bold mb-2">
+                  Aditya Gupta
+                </h2>
+
+                <img
+                  src={aditya_gupta}
+                  alt="Dhrumi Shah"
+                  className="w-[250px] h-[250px]"
+                />
+                <p className="text-white mt-4 mb-4 text-2xl">
+                  <b>Backend & Integrations</b>
+                </p>
+
+                <div className="flex flex-row m-auto justify-center align-center">
+                  <a
+                    href="https://twitter.com/adiig7"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={twitter} alt="Twitter Logo" />
+                  </a>
+                  <a
+                    href="https://github.com/adiig7"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={github} alt="Github Logo" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
