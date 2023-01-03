2bbf5fdbaae5eb497ae0c87fa677b0aad36b66c2
Aditya Gupta <adityagupta765469@gmail.com>
2023-01-03 21:56:07 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../style";
-import style from "../constants/team.css";
+import style from "../styles/team.css";
 import dhrumi_shah from "../assets/dhrumi_shah.jpeg";
 import aditya_gupta from "../assets/aditya_gupta.jpg";
 import twitter from "../assets/twitter.png";
